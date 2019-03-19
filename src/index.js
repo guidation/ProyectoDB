23e37f1e4e9d77e7ebe5b6ef52da877375a08508
Reynaldo Ramirez <reynaldo.ramirez@correo.unimet.edu.ve>
2019-03-19 16:01:16 -0400

    update reynaldo

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ app.use((req, res, next) =>{
 //routes
 app.use(require('./routes'));
 app.use(require('./routes/aunthentication'));
-app.use('/links',require('./routes/links'), require('./routes/aero'), require('./routes/fly'), require('./routes/boletos'));
+app.use('/links', require('./routes/links'), require('./routes/aero'), require('./routes/fly'), require('./routes/boletos'), require('./routes/client'), require('./routes/mantenimiento'));
 
 
 //publicos
