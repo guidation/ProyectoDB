06fae854aea99ce45958c45934cda5de33ee1ee2
Reynaldo Ramirez <reynaldo.ramirez@correo.unimet.edu.ve>
2019-03-20 20:31:49 -0400

    vista alquiler

--- src/routes/alquiler.js
+++ src/routes/alquiler.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 
-
-
+//Alquilar Aviones
+router.get('/alquiler', async (req, res) => {
+    res.render('links/alquiler', { model, state, plane });
+});
 
 module.exports = router;
